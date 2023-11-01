3774dd48bd3bbda3d2c07c761ccdf850eb31c6ae
Hrithik <hrithikvashishtha006@gmail.com>
2023-11-02 01:11:12 +0530

    #File structure

---
+++ chess_bot.py
@@ -0,0 +1,6 @@
+import chess
+from stockfish import Stockfish
+class Stockfish():
+    def engine(self):
+        # Initialize the Stockfish engine
+        stockfish = Stockfish("C:/ProgramData/stockfish/stockfish-windows-x86-64-modern.exe")
--- engine.py
+++
@@ -1,8 +0,0 @@
-import chess
-from stockfish import Stockfish
-
-class Stockfish():
-    def engine(self):
-    # Initialize the Stockfish engine
-    stockfish = Stockfish("C:/ProgramData/stockfish/stockfish-windows-x86-64-modern.exe")
-    print(stockfish)
