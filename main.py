7d45d7df07b15c64b8348f4bb70c000cb4b19725
Hrithik <hrithikvashishtha006@gmail.com>
2023-11-13 18:04:14 +0530

    #Update

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from chess_bot import ChessEngine
 import chess
+
+chess_engine = ChessEngine()
+
+initial_position = chess.STARTING_FEN   
+chess_engine.set_position(initial_position)
+
+best_move = chess_engine.get_best_move()
+
+print(f"The best move is: {best_move}")
+
+def best_move():
+    pass
