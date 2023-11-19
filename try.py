8db954544fdce4442601f72f93b61f407967a8d3
Hrithik <hrithikvashishtha006@gmail.com>
2023-11-19 12:13:23 +0530

    #update

--- try.py
+++ try.py
@@ -1,17 +1,19 @@
-from stockfish import Stockfish
+# from stockfish import Stockfish
 
-stockfish = Stockfish("C:/ProgramData/stockfish/stockfish-windows-x86-64-modern.exe")
+# stockfish = Stockfish("C:/ProgramData/stockfish/stockfish-windows-x86-64-modern.exe")
 
-# Initialize the Stockfish engine
-stockfish.set_fen_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+# # Initialize the Stockfish engine
+# stockfish.set_fen_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
-# Get the board visual representation
-board_visual = stockfish.get_board_visual()
-print(board_visual)  # Print initial board
+# # Get the board visual representation
+# board_visual = stockfish.get_board_visual()
+# print(board_visual)  # Print initial board
 
-# Make a move
-stockfish.make_moves_from_current_position(['e2e4'])
+# # Make a move
+# stockfish.make_moves_from_current_position(['e2e4'])
 
-# Get the updated board visual representation after the move
-board_visual_after_move = stockfish.get_board_visual()
-print(board_visual_after_move)  # Print updated board after move
+# # Get the updated board visual representation after the move
+# board_visual_after_move = stockfish.get_board_visual()
+# print(board_visual_after_move)  # Print updated board after move
+
+
