e3ee5d828fd5c272b05635a276ac796a9150aaee
Hrithik <hrithikvashishtha006@gmail.com>
2023-11-19 12:52:40 +0530

    #update

--- text_to_speech_utils.py
+++ text_to_speech_utils.py
@@ -18,10 +18,19 @@ import pyttsx3
 class TextToSpeech:
     def __init__(self):
         self.engine = pyttsx3.init()
-        self.engine.setProperty('rate', 120)  # Set speech rate here
+        voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice', voices[1].id)  # Set speech rate here
 
     def speech(self, move):
-        voices = self.engine.getProperty('voices')
-        self.engine.setProperty('voice', voices[1].id)  # Set voice here
-        self.engine.say("The move played by the opponent is " + move)
+        self.engine.setProperty('rate', 120)
+        self.engine.say(f"The move played by the opponent is")
+        self.engine.runAndWait()
+        
+        # Second line spoken at rate 100
+        self.engine.setProperty('rate', 80)
+        self.engine.say(move)
+        self.engine.runAndWait()
+
+        self.engine.setProperty('rate', 120)
+        self.engine.say("Please play your move")
         self.engine.runAndWait()
