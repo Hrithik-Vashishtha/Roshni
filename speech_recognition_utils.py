b3fc4cf50e7453a0656dc8012e72280aaadb4919
Hrithik-Vashishtha <hrithikvashishtha006@gmail.com>
2023-12-12 16:09:00 +0530

    #update

--- speech_recognition_utils.py
+++ speech_recognition_utils.py
@@ -1,15 +1,33 @@
 import speech_recognition as sr
 
-
 class SpeechRecognition:
     def audio_input(self):
         r = sr.Recognizer()
+        r.dynamic_energy_threshold = False  # Disable dynamic energy threshold
         with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source)  # Adjust for ambient noise
             print("Say Something")
             audio = r.listen(source)
             print("Time over, thank you")
+
             try:
                 text = r.recognize_google(audio)
-                return text.lower().replace(" ","")
-            except:
+                return text.lower().replace(" ", "")
+            except sr.UnknownValueError:
                 print("Sorry, I didn't get this")
+                return None  # Return None when speech cannot be recognized
+
+
+
+# class SpeechRecognition:
+#     def audio_input(self):
+#         r = sr.Recognizer()
+#         with sr.Microphone() as source:
+#             print("Say Something")
+#             audio = r.listen(source)
+#             print("Time over, thank you")
+#             try:
+#                 text = r.recognize_google(audio)
+#                 return text.lower().replace(" ","")
+#             except:
+#                 print("Sorry, I didn't get this")
